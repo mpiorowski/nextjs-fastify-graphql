db1cd37c515f9ebff244ae00fad61f502915f604
mpiorowski <mateuszpiorowski@gmail.com>
2021-05-20 00:20:21 +0200

    cleanign replies

--- client/src/pages/forum/categories/[categoryId]/topics/[topicId]/PostContent.tsx
+++ client/src/pages/forum/categories/[categoryId]/topics/[topicId]/PostContent.tsx
@@ -11,49 +11,54 @@ interface Props {
 
 export default function PostContent({ post, onOpen, setReplyId }: Props): ReactElement {
   return (
-    <Grid background="gray.800" padding="20px">
+    <Grid background="gray.800" padding="30px 20px 30px 20px" gridTemplateColumns="auto 100px">
       <GridItem fontSize="xs" color="gray.400">
-        Matuesz Piórowski - 2021-01-02 11:33:22
+        <Box>Matuesz Piórowski - 2021-01-02 11:33:22</Box>
+        <Box fontSize="lg" color="whiteAlpha.800">
+          {post.content}
+        </Box>
       </GridItem>
-      <GridItem fontSize="lg">
-        <Box marginBottom="20px">{post.content}</Box>
-        <Box>
-          <Accordion allowMultiple colorScheme="blackAlpha" justifyContent="space-between" display="flex">
-            <AccordionItem border="none">
-              <AccordionButton width="100px" fontSize="14" p="2px" height="32px">
-                <Box flex="1" textAlign="left">
-                  Odpowiedzi
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-              <AccordionPanel p={0} paddingLeft="40px" marginTop="20px">
-                <Grid background="gray.800">
+      <GridItem>
+        <Button
+          size="sm"
+          onClick={() => {
+            setReplyId();
+            onOpen();
+          }}
+        >
+          Odpowiedz
+        </Button>
+      </GridItem>
+      {post.replies.length > 0 && (
+        <GridItem colSpan={2} marginTop="20px">
+          <Accordion allowMultiple colorScheme="blackAlpha">
+            <AccordionItem border="none" width="100%">
+              <h2>
+                <AccordionButton width="120px" fontSize="14" p="2px" height="30px" color="whiteAlpha.800">
+                  <Box flex="1" textAlign="left">
+                    Odpowiedzi ({post.replies.length})
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel p={0} marginTop="20px" marginLeft="20px" width="100%">
+                <Grid background="rgba(160, 155, 155, 0.329)" rowGap="0.5" paddingTop="0.5" paddingBottom="0.5">
                   {post.replies.map((reply) => (
-                    <>
-                      <GridItem fontSize="xs" color="gray.400">
+                    <Box background="gray.800" padding="30px 20px 30px 20px">
+                      <Box fontSize="xs" color="gray.400">
                         Matuesz Piórowski - 2021-01-02 11:33:22
-                      </GridItem>
-                      <GridItem fontSize="lg">
+                      </Box>
+                      <Box fontSize="lg">
                         <Box>{reply.content}</Box>
-                      </GridItem>
-                    </>
+                      </Box>
+                    </Box>
                   ))}
                 </Grid>
               </AccordionPanel>
             </AccordionItem>
-
-            <Button
-              size="sm"
-              onClick={() => {
-                setReplyId();
-                onOpen();
-              }}
-            >
-              Odpowiedz
-            </Button>
           </Accordion>
-        </Box>
-      </GridItem>
+        </GridItem>
+      )}
     </Grid>
   );
 }
