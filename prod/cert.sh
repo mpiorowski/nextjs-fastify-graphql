3a2d456a9d035b750a022f1771c2d331b1b5c051
mpiorowski <mateuszpiorowski@gmail.com>
2021-05-23 22:54:44 +0200

    certs

---
+++ prod/cert.sh
@@ -0,0 +1,5 @@
+sudo snap install core && \
+sudo snap refresh core && \
+sudo snap install --classic certbot && \
+sudo ln -s /snap/bin/certbot /usr/bin/certbot && \
+sudo certbot certonly --standalone
