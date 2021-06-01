fd9a1f48ea5d16faea54cd2dde5bc816c869cecc
mpiorowski <mateuszpiorowski@gmail.com>
2021-06-02 00:23:51 +0200

    fix

--- client/src/pages/_app.page.tsx
+++ client/src/pages/_app.page.tsx
@@ -2,25 +2,22 @@ import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { SubscriptionClient } from "subscriptions-transport-ws";
-import { createClient, defaultExchanges, Provider, subscriptionExchange } from "urql";
-import { w3cwebsocket as W3CWebSocket } from "websocket";
 import "../styles/globals.css";
 
 // TODO - add authprization
-const subscriptionClient = new SubscriptionClient(`ws://server:4000/graphql`, { reconnect: true }, W3CWebSocket);
+// const subscriptionClient = new SubscriptionClient(`ws://server:4000/graphql`, { reconnect: true }, W3CWebSocket);
 
-const client = createClient({
-  url: "http://localhost:3000/api",
-  exchanges: [
-    ...defaultExchanges,
-    subscriptionExchange({
-      forwardSubscription(operation) {
-        return subscriptionClient.request(operation);
-      },
-    }),
-  ],
-});
+// const client = createClient({
+//   url: "http://localhost:3000/api",
+//   exchanges: [
+//     ...defaultExchanges,
+//     subscriptionExchange({
+//       forwardSubscription(operation) {
+//         return subscriptionClient.request(operation);
+//       },
+//     }),
+//   ],
+// });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClientRef = React.useRef<QueryClient>();
@@ -37,20 +34,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <Provider value={client}>
-      <QueryClientProvider client={queryClientRef.current}>
-        <ChakraProvider>
-          <ColorModeProvider
-            options={{
-              // useSystemColorMode: true,
-              initialColorMode: "dark",
-            }}
-          >
-            <Component {...pageProps} />
-          </ColorModeProvider>
-        </ChakraProvider>
-      </QueryClientProvider>
-    </Provider>
+    // <Provider value={client}>
+    <QueryClientProvider client={queryClientRef.current}>
+      <ChakraProvider>
+        <ColorModeProvider
+          options={{
+            // useSystemColorMode: true,
+            initialColorMode: "dark",
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
+    // </Provider>
   );
 }
 
